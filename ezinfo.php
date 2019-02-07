2020da818d0d439373acfdfcd0eb3573c6d7f2ce
Brookins Consulting <info@brookinsconsulting.com>
2019-02-07 13:11:04 -0800

    Updated: Updated version number and copyright to reflect continued maintenance

--- ezinfo.php
+++ ezinfo.php
@@ -1,12 +1,12 @@
 <?php
 class bcstateselectInfo
 {
-    function info()
+    public static function info()
     {
         return array(
             'Name' => "<a href='http://projects.ez.no/bcstateselect'>BC State Select</a>",
-            'Version' => "0.0.1",
-            'Copyright' => "Copyright (C) 2001 - 2007 <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
+            'Version' => "0.0.2",
+            'Copyright' => "Copyright (C) 2001 - 2019 <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
             'Author' => "Brookins Consulting",
             'License' => "GNU General Public License"
         );
